8a16a4dcb8b5e4b26eb548d8b7fa0bd999edb64c
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-20 11:26:22 +0430

    Add register function to view

--- account/views.py
+++ account/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username, password=password).exists():
+            messages.error(request, "Username already exists.")
+        else:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            messages.success(request, "Registration successful.")
+            return redirect("login")
+
+    return render(request, "register.html")
