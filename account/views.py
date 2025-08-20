060696d473301833d2661e6db0dd2f91b3c6ac1f
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-20 13:30:25 +0430

    Add user_login function.

--- account/views.py
+++ account/views.py
@@ -18,3 +18,18 @@ def register(request):
             return redirect("login")
 
     return render(request, "register.html")
+
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, "login.html")
